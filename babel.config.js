6c13f71c1439f0fb6a99e32e7abeb9ac99a1002b
Orji Chidi <orjichidi95@gmail.com>
2019-09-17 13:09:12 +0100

    add remove prop-types

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
 	api.cache(true);
 
 	const presets = [ "@babel/preset-env", "@babel/preset-react" ];
-	const plugins = [ ];
+	const plugins = [ "transform-react-remove-prop-types" ];
 
 	return {
 		presets,
